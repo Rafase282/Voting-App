8626778de4232eabdcf08ba6876445a6f6a5bd7c
Rafael J. Rodriguez <rafase282@gmail.com>
2015-12-11 03:04:20 +0000

    Trying to add react

---
+++ app/src/components/header.js
@@ -0,0 +1,10 @@
+//var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h2> Voting App</h2>,
+  <h4>Create custom polls with live results.</h4>,
+  <button type="button" class="btn btn-success">Sign up</button>,
+  document.getElementById('header')
+);
+

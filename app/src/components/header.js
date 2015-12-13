de5db117c391835233fbe4a26c6e2e50a8760180
Rafael J. Rodriguez <rafase282@gmail.com>
2015-12-13 23:46:35 +0000

    Changed heading tags

--- app/src/components/header.js
+++ app/src/components/header.js
@@ -4,8 +4,8 @@ var HeaderArea = React.createClass({
   render: function() {
     return (
       <section className="jumbotron center">
-      <h2> Voting App</h2>
-      <h4>Create custom polls with live results.</h4>
+      <h1> Voting App</h1>
+      <h2>Create custom polls with live results.</h2>
       <button type="button" className="btn btn-success">Sign up</button>
       </section>
     );

ae4737fdddfb88a29bbf4a4c3dc69aea7824e276
Rafael J. Rodriguez <rafase282@gmail.com>
2016-04-24 18:56:54 -0400

    Adds js

---
+++ public/javascripts/index.js
@@ -0,0 +1,8 @@
+/*eslint no-unused-vars: ["error", { "varsIgnorePattern": "Up || manga" }]*/
+/*global cleanMangas mangaInfo cleanUser */
+'use strict';
+
+/* Load Manga info When Page Is Loaded: When the profile page is read it will call the function to get all mangas. */
+$(document).ready(function () {
+    $('.button-collapse').sideNav();
+});

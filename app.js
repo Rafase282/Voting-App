e6cf32aa31b06b4cf807f441cc81057d8a7790ab
Rafael J. Rodriguez <rafase282@gmail.com>
2016-04-25 00:29:20 -0400

    Implementing passwordless part 1

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require('body-parser');
-
+var passwordless = require('passwordless');
+var MongoStore = require('passwordless-mongostore');
+var email = require("emailjs");
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -18,11 +21,68 @@ app.set('view engine', 'pug');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
+app.use(session({
+  secret: process.env.secret,
+  resave: true,
+  saveUninitialized: false,
+  resave: false,
+  cookie: {
+    maxAge: 3600000
+  }
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, '/public'),
+  dest: path.join(__dirname, '/public'),
+  prefix: '/stylesheets',
+  debug: true,
+  indentedSyntax: true,
+  sourceMap: true
+}));
+
+// Email setting
+var smtpServer = email.server.connect({
+  user: process.env.email,
+  password: process.env.epwd,
+  host: process.env.smtp,
+  ssl: true
+});
+
+// MongoDB TokenStore
+passwordless.init(new MongoStore(process.env.mongoURI));
+
+// Set up a delivery service
+passwordless.addDelivery(
+  function(tokenToSend, uidToSend, recipient, callback) {
+    var host = 'localhost:3000';
+    smtpServer.send({
+      text: 'Hello!\nAccess your account here: http://' + host + '?token=' + tokenToSend + '&uid=' + encodeURIComponent(uidToSend),
+      from: process.env.email,
+      to: recipient,
+      subject: 'Token for ' + host
+    }, function(err, message) {
+      if (err) {
+        console.log(err);
+      }
+      callback(err);
+    });
+  });
+
+// passwordless middleware
+app.use(passwordless.sessionSupport());
+app.use(passwordless.acceptToken({
+  successRedirect: '/'
+}));
+
+
+
 app.use('/', routes);
+app.use('/polls', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var passwordless = require('passwordless');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Voting App' });
+  res.render('index', {
+    title: 'Voting App'
+  });
 });
+
+/* GET polls page. */
+router.get('/polls', function(req, res, next) {
+  res.render('polls', {
+    title: 'Voting App: Polls'
+  });
+});
+
+/* GET login screen. */
+router.get('/login', function(req, res) {
+  res.render('login', {
+    title: 'Voting App: Login'
+  });
+});
+
+/* POST login details. */
+router.post('/sendtoken',
+  passwordless.requestToken(
+    // Turn the email address into an user ID
+    function(user, delivery, callback, req) {
+      // usually you would want something like:
+      User.find({
+          email: user
+        }, callback(ret) {
+          if (ret)
+            callback(null, ret.id)
+          else
+            callback(null, null)
+        })
+        // but you could also do the following
+        // if you want to allow anyone:
+        // callback(null, user);
+    }),
+  function(req, res) {
+    // success!
+    res.render('sent');
+  });
 
 module.exports = router;
